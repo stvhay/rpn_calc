c22c007e825a8751368f14779ac21f9feee5adbe
tablador <wintermute_77@yahoo.com>
2018-09-06 23:28:11 -0700

    Update rpn_calc.rb

--- rpn_calc.rb
+++ rpn_calc.rb
@@ -1,18 +1,16 @@
 # RPN Calculator.
-# Symbols send arg2 to arg1.
 # Works as expected for arithmetic with :+, :-, :/, :*
 def rpn_calc args
-  acc = []
-  for arg in args
-    case arg
-    when Symbol
+  result = args.each_with_object [] do |arg, acc|
+    acc << if arg.instance_of? Symbol
       v2 = acc.pop
       v1 = acc.pop
-      acc.push(v1.send arg, v2)
+      raise 'Invalid computation. v2=#{v2}' if not v1
+      v1.public_send arg, v2
     else
-      acc.push arg
+      arg
     end
   end
-  raise if acc.length != 1
-  acc.first
+  raise 'Invalid computation. result=#{result}' if result.length != 1
+  result.first
 end
